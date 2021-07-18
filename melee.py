c006d3dccbb21fd6901fbc595923b8aba2679727
SensorLock <72746413+SensorLock@users.noreply.github.com>
2021-07-18 12:02:08 -0400

    Parameterize melee strategies
    Re-worked melee code so that melee strategies are much easier to implement by adding a new method to the Strategy class.

--- melee.py
+++ melee.py
@@ -1,194 +1,102 @@
+from ast import Str
 from collections import namedtuple
 from itertools import product
 import numpy as np
 
 Fighter = namedtuple("Fighter", "name num to_hit to_crit dmg dmg_crit mws fnp wounds")
 
-# TODO OPEN Q: are all hits resolved? or just until one model dies?
-# TODO OPEN Q: how are opposing crits parried? can you use 2 hits to parry a crit?
 
-class StrategyBerzerker:
-    def __init__(self, attacker, defender):
+class Strategy:
+    def __init__(self, attacker, defender, strategy_attacker, strategy_defender):
         self.attacker = attacker
         self.defender = defender
+        self.strategy_attacker = strategy_attacker
+        self.strategy_defender = strategy_defender
+
+    def __str__(self):
+        return f"[Strategy: {self.strategy_attacker.__name__} into {self.strategy_defender.__name__}]"
 
     def __call__(self, params):
         a_crits, a_hits, a_wounds_taken, d_crits, d_hits, d_wounds_taken = params
 
+        # TODO OPEN Q: are all hits resolved? or just until one model dies?
+        # TODO OPEN Q: how are opposing crits parried? can you use 2 hits to parry a crit?
         while (a_crits + a_hits + d_crits + d_hits) > 0:
             if a_wounds_taken >= self.attacker.wounds:
                 break
-
-            # BLOCK NOTHING!
-            if a_crits > 0:
-                a_crits -= 1
-                d_wounds_taken += self.attacker.dmg_crit
-            elif a_hits > 0:
-                a_hits -= 1
-                d_wounds_taken += self.attacker.dmg
+            
+            a_crits, a_hits, d_crits, d_hits, d_wounds_taken = self.strategy_attacker(attacker, defender,
+                                                                                      a_crits, a_hits, d_crits, d_hits, d_wounds_taken)
             
             if d_wounds_taken >= self.defender.wounds:
                 break
 
-            # BLOCK NOTHING!
-            if d_crits > 0:
-                d_crits -= 1
-                a_wounds_taken += self.defender.dmg_crit
-            elif d_hits > 0:
-                d_hits -= 1
-                a_wounds_taken += self.defender.dmg
+            d_crits, d_hits, a_crits, a_hits, a_wounds_taken = self.strategy_defender(defender, attacker,
+                                                                                      d_crits, d_hits, a_crits, a_hits, a_wounds_taken)
         
         return a_wounds_taken, d_wounds_taken
 
 
-class StrategyAttackerParry:
-    def __init__(self, attacker, defender):
-        self.attacker = attacker
-        self.defender = defender
-
-    def __call__(self, params):
-        a_crits, a_hits, a_wounds_taken, d_crits, d_hits, d_wounds_taken = params
-
-        while (a_crits + a_hits + d_crits + d_hits) > 0:
-            if a_wounds_taken >= self.attacker.wounds:
-                break
-
-            # PARRY EVERYTHING! (unless there is a lethal riposte)!
-            if a_crits > 0:
-                a_crits -= 1
-                if d_wounds_taken + self.attacker.dmg_crit >= self.defender.wounds:
-                    d_wounds_taken += self.attacker.dmg_crit
-                elif d_crits > 0:
-                    d_crits -= 1
-                elif d_hits > 0:
-                    d_hits -= 1
-                else:
-                    d_wounds_taken += self.attacker.dmg_crit
-            elif a_hits > 0:
-                a_hits -= 1
-                if d_wounds_taken + self.attacker.dmg >= self.defender.wounds:
-                    d_wounds_taken += self.attacker.dmg
-                elif d_hits > 0:
-                    d_hits -= 1
-                else:
-                    d_wounds_taken += self.attacker.dmg
+    @staticmethod
+    def berzerker(me, opp, my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken):
+        # BLOCK NOTHING!
+        if my_crits > 0:
+            my_crits -= 1
+            opp_wounds_taken += me.dmg
+        elif my_hits > 0:
+            my_hits -= 1
+            opp_wounds_taken += me.dmg_crit
             
-            if d_wounds_taken >= self.defender.wounds:
-                break
+        return my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken
 
-            # BLOCK NOTHING!
-            if d_crits > 0:
-                d_crits -= 1
-                a_wounds_taken += self.defender.dmg_crit
-            elif d_hits > 0:
-                d_hits -= 1
-                a_wounds_taken += self.defender.dmg
+
+    @staticmethod
+    def parry_riposte(me, opp, my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken):
+        # PARRY EVERYTHING! (unless there is a lethal riposte)!
+        if my_crits > 0:
+            my_crits -= 1
+            if opp_wounds_taken + me.dmg_crit >= opp.wounds:
+                opp_wounds_taken += me.dmg_crit
+            elif opp_crits > 0:
+                opp_crits -= 1
+            elif opp_hits > 0:
+                # TODO OPEN Q: crit conversion for melee
+                opp_hits -= 1
+            else:
+                opp_wounds_taken += me.dmg_crit
+        elif my_hits > 0:
+            my_hits -= 1
+            if opp_wounds_taken + me.dmg >= opp.wounds:
+                opp_wounds_taken += me.dmg
+            elif opp_hits > 0:
+                opp_hits -= 1
+            else:
+                opp_wounds_taken += me.dmg
+
+        return my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken
+
+
+    @staticmethod
+    def aggressive(me, opp, my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken):
+        # Go for the kill, but fall back to parrying if you don't have enough damage left to kill
+        # This doesn't consider ahead of time the number of available parries the opponent may take
+        if my_crits * me.dmg_crit + my_hits * me.dmg + opp_wounds_taken > opp.wounds:
+            return Strategy.berzerker(me, opp, my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken)
+        else:
+            return Strategy.parry_riposte(me, opp, my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken)
+
+    @staticmethod
+    def overpower(me, opp, my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken):
+        # If you have enough crits+hits to push through all their parries to kill then go on the attack
+        usable_crits = max(my_crits - opp_crits, 0)
+        # TODO OPEN Q: crit conversion for melee
+        carryover = max(opp_crits - my_hits, 0)
+        usable_hits = max(my_hits - opp_hits - carryover, 0)
         
-        return a_wounds_taken, d_wounds_taken
-
-
-class StrategyDefenderParry:
-    def __init__(self, attacker, defender):
-        self.attacker = attacker
-        self.defender = defender
-
-    def __call__(self, params):
-        a_crits, a_hits, a_wounds_taken, d_crits, d_hits, d_wounds_taken = params
-
-        while (a_crits + a_hits + d_crits + d_hits) > 0:
-            if a_wounds_taken >= self.attacker.wounds:
-                break
-
-            # BLOCK NOTHING!
-            if a_crits > 0:
-                a_crits -= 1
-                d_wounds_taken += self.attacker.dmg_crit
-            elif a_hits > 0:
-                a_hits -= 1
-                d_wounds_taken += self.attacker.dmg
-            
-            if d_wounds_taken >= self.defender.wounds:
-                break
-
-            # PARRY EVERYTHING! (unless there is a lethal riposte)
-            if d_crits > 0:
-                d_crits -= 1
-                if a_wounds_taken + self.defender.dmg_crit >= self.attacker.wounds:
-                    a_wounds_taken += self.defender.dmg_crit
-                elif a_crits > 0:
-                    a_crits -= 1
-                elif a_hits > 0:
-                    a_hits -= 1
-                else:
-                    a_wounds_taken += self.defender.dmg_crit
-            elif d_hits > 0:
-                d_hits -= 1
-                if a_wounds_taken + self.defender.dmg >= self.attacker.wounds:
-                    a_wounds_taken += self.defender.dmg
-                elif a_hits > 0:
-                    a_hits -= 1
-                else:
-                    a_wounds_taken += self.defender.dmg
-        
-        return a_wounds_taken, d_wounds_taken
-
-
-class StrategyMaxParry:
-    def __init__(self, attacker, defender):
-        self.attacker = attacker
-        self.defender = defender
-
-    def __call__(self, params):
-        a_crits, a_hits, a_wounds_taken, d_crits, d_hits, d_wounds_taken = params
-
-        while (a_crits + a_hits + d_crits + d_hits) > 0:
-            if a_wounds_taken >= self.attacker.wounds:
-                break
-
-            # PARRY EVERYTHING! (unless there is a lethal riposte)!
-            if a_crits > 0:
-                a_crits -= 1
-                if d_wounds_taken + self.attacker.dmg_crit >= self.defender.wounds:
-                    d_wounds_taken += self.attacker.dmg_crit
-                elif d_crits > 0:
-                    d_crits -= 1
-                elif d_hits > 0:
-                    d_hits -= 1
-                else:
-                    d_wounds_taken += self.attacker.dmg_crit
-            elif a_hits > 0:
-                a_hits -= 1
-                if d_wounds_taken + self.attacker.dmg >= self.defender.wounds:
-                    d_wounds_taken += self.attacker.dmg
-                elif d_hits > 0:
-                    d_hits -= 1
-                else:
-                    d_wounds_taken += self.attacker.dmg
-            
-            if d_wounds_taken >= self.defender.wounds:
-                break
-
-            # PARRY EVERYTHING! (unless there is a lethal riposte)
-            if d_crits > 0:
-                d_crits -= 1
-                if a_wounds_taken + self.defender.dmg_crit >= self.attacker.wounds:
-                    a_wounds_taken += self.defender.dmg_crit
-                elif a_crits > 0:
-                    a_crits -= 1
-                elif a_hits > 0:
-                    a_hits -= 1
-                else:
-                    a_wounds_taken += self.defender.dmg_crit
-            elif d_hits > 0:
-                d_hits -= 1
-                if a_wounds_taken + self.defender.dmg >= self.attacker.wounds:
-                    a_wounds_taken += self.defender.dmg
-                elif a_hits > 0:
-                    a_hits -= 1
-                else:
-                    a_wounds_taken += self.defender.dmg
-        
-        return a_wounds_taken, d_wounds_taken
+        if usable_crits * me.dmg_crit + usable_hits * me.dmg + opp_wounds_taken > opp.wounds:
+            return Strategy.berzerker(me, opp, my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken)
+        else:
+            return Strategy.parry_riposte(me, opp, my_crits, my_hits, opp_crits, opp_hits, opp_wounds_taken)
 
 
 def simulate_melee(attacker: Fighter, defender: Fighter, strategy, samples=100000):
@@ -212,7 +120,7 @@ def simulate_melee(attacker: Fighter, defender: Fighter, strategy, samples=10000
     d_wounds = wounds_taken[1]
     d_killed = (d_wounds >= defender.wounds).sum()
 
-    print(f"{attacker.name} fighting {defender.name} using {strategy.__class__.__name__}")
+    print(f"{attacker.name} fighting {defender.name} using {str(strategy)}")
     print(f"Attacker wounds taken distribution: {np.bincount(a_wounds)}")
     print(f"Attacker avg. wounds taken: {a_wounds.mean()}")
     print(f"Attacker killed: {a_killed/samples * 100}%")
@@ -224,10 +132,10 @@ def simulate_melee(attacker: Fighter, defender: Fighter, strategy, samples=10000
 
 fighters = [
     Fighter("Trooper Veteran (Power Weapon)", 4, 3, 5, 4, 6, 0, None, 7),
-    Fighter("Trooper Veteran (Bayonet)", 3, 4, 6, 2, 3, 0, None, 7),
+    # Fighter("Trooper Veteran (Bayonet)", 3, 4, 6, 2, 3, 0, None, 7),
 ]
 
-strategies = [StrategyBerzerker, StrategyAttackerParry, StrategyDefenderParry, StrategyMaxParry]
-for attacker, defender, strategy in product(fighters, fighters, strategies):
-    s = strategy(attacker, defender)
+strategies = [Strategy.berzerker, Strategy.aggressive, Strategy.parry_riposte, Strategy.overpower]
+for attacker, defender, a_strategy, d_strategy in product(fighters, fighters, strategies, strategies):
+    s = Strategy(attacker, defender, a_strategy, d_strategy)
     simulate_melee(attacker, defender, s)
