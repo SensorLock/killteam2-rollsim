53aa7e3491b26a3cbb3c07cb6ccf33eb6b6f0516
SensorLock <72746413+SensorLock@users.noreply.github.com>
2021-07-26 21:00:42 -0400

    AP, Invulns, FNP
    Simplify the FNP code, add support for All is Dust, and handle AP and invulns using the standard 40k approach for now.
    
    Also added a handful of new weapons and targets from preview datacards. That exposed a bug in the placement of the vertical red wound max lines on the plot.

--- ranged.py
+++ ranged.py
@@ -4,11 +4,38 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-Attacker = namedtuple("Attacker", "name num to_hit to_crit dmg dmg_crit mws")
-Defender = namedtuple("Defender", "name num to_save to_critsave fnp wounds")
+Attacker = namedtuple("Attacker", "name num to_hit to_crit dmg dmg_crit ap mws")
+
+weapons = [
+    # Attacker("Frag", 4, 4, 6, 2, 4, 0, 0),
+    # Attacker("Krak", 4, 4, 6, 4, 5, 1, 0),
+    Attacker("Las", 4, 4, 6, 2, 3, 0, 0),
+    Attacker("Long-Las", 4, 2, 6, 3, 3, 0, 3),
+    Attacker("Flamer", 5, 2, 6, 2, 2, 0, 0),
+    Attacker("M Bolt", 4, 3, 6, 3, 4, 0, 0),
+    Attacker("CSM Melta", 4, 3, 6, 6, 3, 2, 4),
+    Attacker("CSM Plasma (Std)", 4, 3, 6, 5, 6, 1, 0),
+    Attacker("CSM Plasma (Over)", 4, 3, 6, 5, 6, 2, 0), # Hot
+    Attacker("Shuriken Catapult", 4, 3, 6, 3, 4, 0, 0),  # SR Balanced? ! Rending?
+    Attacker("Guardian Spear", 4, 2, 6, 3, 5, 0, 0),  # P1
+    Attacker("Sentinel Blade", 4, 2, 6, 3, 4, 0, 0),  # P1
+]
+
+Defender = namedtuple("Defender", "name num to_save to_critsave invuln fnp dust wounds")
+
+targets = [
+    Defender("Trooper Veteran", 3, 5, 6, None, None, False, 7),
+    Defender("Trooper Veteran (Hardened by War)", 3, 5, 6, None, 5, False, 7),
+    Defender("Poxwalker", 3, 6, 6, None, 5, False, 7),
+    Defender("Dire Avenger", 3, 4, 6, None, None, False, 8),
+    Defender("Chaos Space Marine", 3, 3, 6, None, None, False, 12),
+    Defender("Rubric Marine", 3, 3, 6, 5, None, True, 12),
+    Defender("Custodian Guard", 3, 2, 6, None, None, False, 18),
+    Defender("Custodian guard (S)", 3, 2, 6, 4, None, False, 18),
+]
 
 
-def simulate_ranged(attacker: Attacker, defender: Defender, samples=100000):
+def simulate_ranged(attacker: Attacker, defender: Defender, samples=10000):
     # Roll a bunch of dice in atk and def groups
     a_rolls = np.random.choice(6, (samples, attacker.num)) + 1
     d_rolls = np.random.choice(6, (samples, defender.num)) + 1
@@ -17,8 +44,18 @@ def simulate_ranged(attacker: Attacker, defender: Defender, samples=100000):
     mws = crits * attacker.mws
     hits = (a_rolls >= attacker.to_hit).sum(axis=1) - crits
 
+    if defender.dust and attacker.dmg <= 3:
+        save_needed = 2
+    else:
+        save_needed = defender.to_save
+    
+    save_needed += attacker.ap
+    if defender.invuln is not None:
+        save_needed = min(save_needed, defender.invuln)
+
+    # TODO OPEN Q: How do critical saves interact with AP?
     critsaves = (d_rolls >= defender.to_critsave).sum(axis=1)
-    saves = (d_rolls >= defender.to_save).sum(axis=1) - critsaves
+    saves = (d_rolls >= save_needed).sum(axis=1) - critsaves
 
     crits -= critsaves
 
@@ -47,30 +84,13 @@ def simulate_ranged(attacker: Attacker, defender: Defender, samples=100000):
     damage = (mws + crits * attacker.dmg_crit + hits * attacker.dmg)
     
     if defender.fnp is not None:
-        # Get rolls for feel-no-pains equal up max possible wounds, will take prefix of this based on actual wounds
-        fnp_rolls = np.random.choice(6, (samples, damage.max())) + 1
-        fnp_rolls = fnp_rolls >= defender.fnp
-
+        # We need a variable number of rolls for each sample so just loop across the damamge array
         for i in range(samples):
-            # Set rolls that were on wounds beyond what should be checked to False
-            fnp_rolls[i][damage[i]:] = False
-        
-        fnp = fnp_rolls.sum(axis=1)
-        damage -= fnp
+            fnp_rolls = np.random.choice(6, damage[i]) + 1
+            damage[i] -= (fnp_rolls >= defender.fnp).sum()
 
     return damage
 
-
-weapons = [
-    Attacker("Lasgun", 4, 4, 6, 2, 3, 0),
-    Attacker("Long-Las", 4, 2, 6, 3, 3, 3),
-    Attacker("Flamer", 5, 2, 6, 2, 2, 0),
-]
-
-targets = [
-    Defender("Trooper Veteran", 3, 5, 6, None, 7),
-    Defender("Trooper Veteran (Hardened by War)", 3, 5, 6, 5, 7),
-]
 
 if __name__ == "__main__":
     all_dfs = []
@@ -92,7 +112,7 @@ if __name__ == "__main__":
     df = pd.concat(all_dfs)
     fig = px.histogram(df, x="Damage", histnorm="probability", facet_row="Weapon", facet_col="Target")
     for row, col, x, y in kill_probs:
-        fig.add_vline(x=x-0.5, line_color="red", line_dash="dash")
+        fig.add_vline(x=x-0.5, row=row, col=col, line_color="red", line_dash="dash")
         fig.add_hrect(y0=0, y1=y, row=row, col=col, fillcolor="red", opacity=0.2, layer="below")
     fig.show()
     
